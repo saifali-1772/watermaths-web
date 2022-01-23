9d7e936235d495861b77734b5086929fc8982f54
saif ali <saifalishaikh.email@gmail.com>
2022-01-23 18:15:51 +0530

    update all files

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,47 +2,47 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/',(req,res)=>{
-    res.render('index')
+    res.render('index',{title:"Watermaths"})
 })
 
 route.get('/about',(req,res)=>{
-    res.render('about')
+    res.render('about',{title:"About"})
 })
 
 route.get('/contact',(req,res)=>{
-    res.render('contact')
+    res.render('contact',{title:"Contact"})
 })
 
 route.get('/services',(req,res)=>{
-    res.render('services')
+    res.render('services',{title:"Services"})
 })
 
 route.get('/service-details',(req,res)=>{
-    res.render('service-details')
+    res.render('service-details',{title:"Service Details"})
 })
 
 route.get('/portfolio',(req,res)=>{
-    res.render('portfolio')
+    res.render('portfolio',{title:"Portfolio"})
 })
 
 route.get('/portfolio-details',(req,res)=>{
-    res.render('portfolio-details')
+    res.render('portfolio-details',{title:"Portfolio Details"})
 })
 
 route.get('/blog',(req,res)=>{
-    res.render('blog')
+    res.render('blog',{title:"Blog"})
 })
 
 route.get('/blog-details',(req,res)=>{
-    res.render('blog-details')
+    res.render('blog-details',{title:"Blog Details"})
 })
 
 route.get('/customer-info',(req,res)=>{
-    res.render('customer')
+    res.render('customer',{title:"Customer Info"})
 })
 
 route.get('*',(req,res)=>{
-    res.render('page-not-found') 
+    res.render('page-not-found',{title:"404 Error"}) 
 })
 
 module.exports = route
